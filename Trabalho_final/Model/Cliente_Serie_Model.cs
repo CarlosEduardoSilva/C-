3b718b5f6cb01b7aa9de7ae1bf4ccd257a0cc469
RichardTeske <richard25sa@gmail.com>
2018-06-18 09:10:34 -0300

    Criação do objeto Cliente_Serie e adicinando as variaveis

---
+++ Trabalho_final/Model/Cliente_Serie_Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+	class Cliente_Serie_Model
+	{
+
+		public int ID_Cliente_Serie { get; set; }
+
+		public int ID_Cliente { get; set; }
+
+		public int ID_Serie { get; set; }
+
+	}
+}
